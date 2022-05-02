d74176232cba118eb2fd055f0c0b2019699f5019
Caroline Ferraz <projetos.crrz@gmail.com>
2022-05-02 19:44:20 -0300

    adicionando a tabela tb_temas

--- 02_CRUD/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ 02_CRUD/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,32 +6,39 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 3, max = 100)
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 10, max = 1000)
 	private String texto;
-	
+
 	@UpdateTimestamp
 	private LocalDateTime data;
-	
+
 	private String foto;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -72,7 +79,13 @@ public class Postagem {
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
-	
-	
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 }
